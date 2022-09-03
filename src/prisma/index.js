ff009c6b4c379f7447be684cd829ef954ca7caf6
mochamadyudi <myudisobari12@gmail.com>
2022-09-03 21:38:59 +0700

    add start.sh & Dockerfile

--- src/prisma/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.PrismaService = exports.PrismaModule = void 0;
-var prisma_module_1 = require("./prisma.module");
-exports.PrismaModule = prisma_module_1.PrismaModule;
-var prisma_service_1 = require("./prisma.service");
-exports.PrismaService = prisma_service_1.PrismaService;
